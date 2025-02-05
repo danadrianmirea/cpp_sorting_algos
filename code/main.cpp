f5049376732e7af50b243534704acb3fb2313ac3
Dan Adrian Mirea <damirea84@gmail.com>
2025-02-05 18:07:55 +0200

    formatting, use c++23 again

--- code/main.cpp
+++ code/main.cpp
@@ -1,32 +1,32 @@
 #include <iostream>
 #include <vector>
 
-void bubble_sort(std::vector<int>& v)
+void bubble_sort(std::vector<int> &v) 
 {
-    int n = v.size();
-    
-    for( int end = n-1; end>0; end--)
+  int n = v.size();
+
+  for (int end = n - 1; end > 0; end--) 
+  {
+    for (int j = 0; j < end; j++) 
     {
-        for (int j = 0; j < end; j++)
-        {
-            if (v[j] > v[j + 1])
-            {
-                std::swap(v[j], v[j + 1]);
-            }
-        }
+      if (v[j] > v[j + 1]) 
+      {
+        std::swap(v[j], v[j + 1]);
+      }
     }
+  }
 }
 
-int main()
+int main() 
 {
-    std::vector<int> v = { 55, -43, 1, 9, -2 };
-    bubble_sort(v);
+  std::vector<int> v = {55, -43, 1, 9, -2};
+  bubble_sort(v);
 
-    for (auto& e : v)
-    {
-        std::cout << e << " ";
-    }
+  for (auto &e : v) 
+  {
+    std::cout << e << " ";
+  }
 
-    std::cout << "\n";
-    return 0;
+  std::cout << "\n";
+  return 0;
 }
